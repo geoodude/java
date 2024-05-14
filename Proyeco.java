db32250f6fee395e9c66fd99213b486d6e7a4669
geoodude <roc.badia@inslapineda.cat>
2024-05-14 20:20:30 +0200

    yeray

--- Proyeco.java
+++ Proyeco.java
@@ -1,1 +1,16 @@
-# java
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GestorDeLlibres {
+
+    public static String[] llegirLlibres() {
+        StringBuilder contingut = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader("Llibres.txt"))) {
+            String linia;
+            while ((linia = br.readLine()) != null) {
+                contingut.append(linia).append("\n");
+            }
